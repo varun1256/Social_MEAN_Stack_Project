299fab173a2db127ee4860dad05014c2ffdb0cbc
Varun Narang <varunnarang01@gmail.com>
2022-10-09 20:38:08 +0530

    setting user id in post and controller

--- server/controllers/comment.controller.js
+++ server/controllers/comment.controller.js
@@ -1,31 +1,39 @@
-const {Comment}=require('../models');
+const { Comment } = require('../models');
 const { to, ReE, ReS } = require('../services/util.services');
 const logger = require("../lib/logging");
-const {Post}  = require('../models');
+const { Post } = require('../models');
 
-const createComment= async(req,res)=>{
-    let err,comment;
-    if(!req.body.content){
+const createComment = async (req, res) => {
+    let err, comment;
+    if (!req.body.content) {
         logger.error("Comment-Controller :Content is required");
-            return ReE(res, "Comment-Controller:Content is required");
+        return ReE(res, "Comment-Controller:Content is required");
     }
-    if(!req.body.post_id){
+    if (!req.body.post_id) {
         logger.error("Comment-Controller :Post Id is required");
-            return ReE(res, "Comment-Controller:Post Id is required");
+        return ReE(res, "Comment-Controller:Post Id is required");
 
     }
-    [err,comment]=await to(Comment.create(req.body));
-    if(err){
-    return ReE(res, err, 422);
+    [err, comment] = await to(Comment.create(req.body));
+    if (err) {
+        logger.error("Comment-Controller :error in creating comment");
+        return ReE(res, "Comment-Controller:error in creating comment");
     }
+    comment.user_id = req.user.user_id;
+
+    [err, comment] = await to(comment.save());
+    if (err) {
+        return ReE(res, "Comment-Controller:Error in saving comment");
+    }
+
     let post
-    [err,post]=await to(Post.findById(req.body.post_id));
-    if(err){
+    [err, post] = await to(Post.findById(req.body.post_id));
+    if (err) {
         return ReE(res, err, 422);
     }
     post.comments.push(comment._id);
-    [err,savePost]=await to(post.save());
+    [err, savePost] = await to(post.save());
 
-    return ReS(res, { message: "Successfully saved comment", comment: comment}, 201);
-    }
-    module.exports.createComment=createComment;
+    return ReS(res, { message: "Successfully saved comment", comment: comment }, 201);
+}
+module.exports.createComment = createComment;
--- server/controllers/post.controller.js
+++ server/controllers/post.controller.js
@@ -4,11 +4,24 @@ const logger = require("../lib/logging");
 
 const createPost= async(req,res)=>{
 let err,post;
+if(!req.user.user_id){
+  logger.error("Post-Controller :User is not authenticated");
+		return ReE(res, "Post-Controller:User is not authenticated");
+}
 if(!req.body.content){
     logger.error("Post-Controller :Content is required");
 		return ReE(res, "Post-Controller:Content is required");
 }
 [err,post]=await to(Post.create(req.body));
+if(err){
+  logger.error("Post-Controller :error in creating post");
+  return ReE(res, "Post-Controller:error in creating post");
+}
+post.user_id=req.user.user_id;
+[err,post]=await to(post.save());
+if(err){
+  return ReE(res, "Post-Controller:Error in saving post");
+}
 if(err){
 return ReE(res, err, 422);
 }
